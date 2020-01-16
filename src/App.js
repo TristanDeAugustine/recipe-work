73f2c368f8a7cb0128e532f732a26459fe6a2f24
Tristan <deaugustinetristan@gmail.com>
2020-01-16 12:35:44 -0500

    api key working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import React, { useEffect, useState } from 'react'
 
-class App extends Component {
-  render() {
-    return <HelloWorld />
-  }
+const App = () => {
+  const APP_ID = '368135c0'
+  const APP_KEY = 'dbcf9992f815976115e9ae112151268e'
+
+  const exampleReq =
+    'https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}'
+
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    console.log('Effect has been run')
+  })
+
+  return (
+    <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit"></button>
+      </form>
+      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
+    </div>
+  )
 }
 
 export default App
